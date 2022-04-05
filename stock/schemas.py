be9cbe89e6114cbeb693672805c6e9bddef91506
Eduardo Almeida <eapsa@ua.pt>
2022-04-05 20:37:52 +0100

    add querys

--- stock/schemas.py
+++ stock/schemas.py
@@ -2,28 +2,31 @@ from pydantic import BaseModel
 from typing import Optional
 
 class Provider(BaseModel):
-    id: int
     name: str
     contact: int
     mail: str
     class Config:
         orm_mode = True
 
+class UpdateProvider(BaseModel):
+    name: Optional[str] = None
+    contact: Optional[int] = None
+    mail: Optional[str] = None
+
 class Product(BaseModel):
-    id: int
     name: str
     location: str
     price: int
     quantity: int
     type: str
-    provider: Provider
+    provider_id: int
     class Config:
         orm_mode = True
 
 class UpdateProduct(BaseModel):
     name: Optional[str] = None
     location: Optional[str] = None
-    price: Optional[int] = int
-    quantity: Optional[int] = int
+    price: Optional[int] = None
+    quantity: Optional[int] = None
     type: Optional[str] = None
-    provider: Optional[Provider] = None
+    provider_id: Optional[int] = None
