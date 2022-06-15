d7840f2a3a89ab2c72f059a81d23e3ebbef7e1c2
coutooo <manuelcouto10@ua.pt>
2022-06-15 09:58:21 +0100

    integrated with mariaDB

--- payments/api.py
+++
@@ -1,6 +0,0 @@
-import sqlalchemy
-from sqlalchemy.ext.declarative import declarative_base
-
-# Define the MariaDB engine using MariaDB Connector/Python
-
-engine = sqlalchemy.create_engine("mariadb+mariadbconnector://couto:couto123@127.0.0.1:3306/payments")
