c1d3297a20e2347d4b3513d87f2d5334f4b6a4a7
francisco.monteiro <francisco.monteiro@ua.pt>
2022-06-21 11:48:27 -0700

    Added HTTP status code

--- ticketing/main.py
+++ ticketing/main.py
@@ -1,7 +1,7 @@
 #run this program with python3 -m uvicorn main:app --reload on Windows
 #or just uvicorn main:app --reload on Linux
 
-from fastapi import Depends, FastAPI    
+from fastapi import Depends, FastAPI, status    
 from fastapi_versioning import VersionedFastAPI, version
 from sqlalchemy.orm import Session
 from typing import List
@@ -20,9 +20,9 @@ def get_db():
     finally:
         db.close()
 
-# @app.get("/")
-# def hello_world():
-#     return {"Success": "Hello World"}
+@app.get("/")
+def hello_world():
+    return {"Success": "Hello World"}
 
 @app.get("/event/{event_id}", response_model=schemas.Event)
 @version(1)
@@ -34,7 +34,7 @@ def get_event_by_id(event_id: int, db: Session = Depends(get_db)):
 def get_events(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return crud.get_events(db, skip, limit)
 
-@app.post("/event/", response_model=schemas.Event)
+@app.post("/event/", response_model=schemas.Event, status_code=status.HTTP_201_CREATED)
 @version(1)
 def create_event(event: schemas.Event, db: Session = Depends(get_db)):
     return crud.create_event(db, event)
@@ -49,10 +49,10 @@ def update_event(event_id: int, event: schemas.UpdateEvent, db: Session = Depend
 def delete_event(event_id: int, db: Session = Depends(get_db)):
     return crud.delete_event(db, event_id)
 
-@app.post("/ticket/")
-@version(1)
-def create_ticket(ticket: schemas.Ticket, db: Session = Depends(get_db)):
-    return crud.create_ticket(db, ticket)
+# @app.post("/ticket/")
+# @version(1)
+# def create_ticket(ticket: schemas.Ticket, db: Session = Depends(get_db)):
+#     return crud.create_ticket(db, ticket)
 
 @app.get("/ticket/{ticket_id}")
 @version(1)
@@ -69,11 +69,10 @@ def get_tickets(nif: int = None, status: int = None, name: str = None, event_id:
 def update_ticket(ticket_id: int, ticket: schemas.UpdateTicket, db: Session = Depends(get_db)):
     return crud.update_ticket(db, ticket, ticket_id)
 
-@app.post("/ticket/{ticket_id}/pay/")
+@app.post("/ticket/pay/")
 @version(1)
-def pay_ticket(ticket_id: int):
-    #paymentService.pay(ticket_id, amount, nif)
-    return {"Success": "Ticket paid successfully"}
+def pay_ticket(event_id: int, nif: int, name: str, db: Session = Depends(get_db)):
+    return crud.pay_ticket(db, event_id, nif, name)
 
 app = VersionedFastAPI(app,
     version_format='{major}',
