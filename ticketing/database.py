1f4007e6c621348acf6722bdb3f8dc71c28481ea
francisco.monteiro <francisco.monteiro@ua.pt>
2022-06-22 06:28:10 -0700

    Added secret

--- ticketing/database.py
+++ ticketing/database.py
@@ -4,15 +4,10 @@ from sqlalchemy.orm import sessionmaker
 from pydantic import Field
 import os
 
-
-# POSTGRES_USER = str(Field(..., env='POSTGRES_USER'))
-# POSTGRES_PASSWORD = str(Field(..., env='POSTGRES_PASSWORD'))
-# POSTGRES_SERVER = str(Field(..., env='POSTGRES_SERVER'))
-#POSTGRES_PORT = str(Field(..., env='POSTGRES_PORT'))
-# POSTGRES_DB = str(Field(..., env='POSTGRES_DB'))
-
-#SQLALCHEMY_DATABASE_URL = "postgresql://" + POSTGRES_USER + ":" + POSTGRES_PASSWORD + "@" + POSTGRES_SERVER + ":5432/" + POSTGRES_DB
-SQLALCHEMY_DATABASE_URL = "postgresql://francisco:ticketing@db-ticketing:5432/ticketingdb"
+SQLALCHEMY_DATABASE_URL = ""
+#SQLALCHEMY_DATABASE_URL = "postgresql://francisco:ticketing@db-ticketing:5432/ticketingdb"
+with open("/tmp/secrets/ticketing-secret", 'r') as f:
+    SQLALCHEMY_DATABASE_URL = f.read().strip()
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
